0b93f09557b832fa82db445c397ce2b5e65f9c7f
monicasoberon <a01254488@tec.mx>
2024-03-09 15:41:01 -0600

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Monica Soberon on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
